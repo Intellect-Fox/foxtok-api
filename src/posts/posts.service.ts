eb26becef403c2435fd104db95078525b0db8338
Andrei Bahamolau <abogomolov90@gmail.com>
2023-03-16 00:20:40 +0100

    Fixed token validation in postsService

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -6,11 +6,11 @@ import { AuthService } from '../auth/auth.service';
 export class PostsService {
   constructor(private authService: AuthService) {}
   getPosts(accessToken: string): Post[] {
-    try {
-      this.authService.validateToken(accessToken);
-      return posts.sort(() => Math.random() - 0.5);
-    } catch (err) {
+    if (!accessToken) {
       return posts;
     }
+
+    this.authService.validateToken(accessToken);
+    return posts.sort(() => Math.random() - 0.5);
   }
 }
